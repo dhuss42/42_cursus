1fa6a10d3db44d64439312b3bf32bf9ed8da2616
david <david.david@david.com>
2024-10-01 13:58:01 +0200

    function works now, needs adjustment for len

--- sources/insert_trim_ws.c
+++ sources/insert_trim_ws.c
@@ -5,99 +5,141 @@
 #include "libft/libft.h"
 #include <stdbool.h>
 
-void	handle_double_op(char *input, char *res, size_t *i, size_t *j)
+typedef struct s_trim
 {
-	if (*j > 0 && res[*j - 1] != ' ')
-		res[(*j)++] = ' ';
-	res[(*j)++] = input[*i];
-	res[(*j)++] = input[*i + 1];
-	if (input[*i + 2] != ' ')
-		res[(*j)++] = ' ';
-	(*i)++;
+	size_t	i;
+	size_t	j;
+	size_t	len;
+	bool	isspace;
+	char	*res;
+} t_trim;
+
+bool	isspecial(char input)
+{
+	char *special;
+
+	special = "<>|\'\"$";
+	while (*special != '\0')
+	{
+		if (input == *special)
+			return (true);
+		special++;
+	}
+	return (false);
 }
 
-void	handle_op(char *input, char *res, size_t *i, size_t *j)
+bool	iswspace(char input)
 {
-	if (*j > 0 && res[*j - 1] != ' ')
-		res[(*j)++] = ' ';
-	res[(*j)++] = input[*i];
-	if (input[*i + 2] != ' ')
-		res[(*j)++] = ' ';
+	char	*ws;
+
+	ws = "\n\t ";
+	while (*ws != '\0')
+	{
+		if (input == *ws)
+			return (true);
+		ws++;
+	}
+	return (false);
 }
 
-void handle_ws_quotes(char *input, char *res, size_t *i, size_t *j)
+void handle_quotes(t_trim *trim, char *input)
 {
 	char quote;
 
-	quote = input[*i];
-	res[(*j)++] = input[(*i)++];
-	while ((input[*i] != '\0') && (input[*i] != quote)) {
-		res[(*j)++] = input[(*i)++];
-	}
-	if (input[*i] == quote)
+	if (trim->j > 0 && (!iswspace(trim->res[trim->j - 1]))) // if not the beginning of res and the prior space is not ws
+		trim->res[trim->j++] = ' ';
+	quote = input[trim->i];
+	trim->res[trim->j++] = input[trim->i++];
+	while ((input[trim->i] != '\0') && (input[trim->i] != quote)) 
+		trim->res[trim->j++] = input[trim->i++];
+	if (input[trim->i] == quote)
 	{
-		printf("* Final quote found: `%c`\n", input[*i]);
-		res[(*j)++] = input[(*i)];
-		printf("* After: `%c`\n", input[*i]);
-		printf("* RES: `%s`\n", res);
-		if (input[*i] != ' ' && input[*i] != '\0') {
-			printf("* I add space @ [%d]\n", *j);
-			res[(*j)++] = ' ';
-		}
+		trim->res[trim->j++] = input[trim->i];
+			trim->res[trim->j++] = ' ';
 	}
 	else
 		printf("error\n"); // update here
 }
 
-
-char *trim_spaces(char *input)
+void	handle_op(t_trim *trim, char *input)
 {
-	size_t	len;
-	size_t	j;
-	size_t	i;
-	bool	isspace;
-	char	*res;
-
-	if (!input)
-		return (NULL);
-	len = ft_strlen(input);
-	res = malloc(sizeof(char) * (len + 1));
-	if (!res)
-		return (NULL);
-	j = 0;
-	i = 0;
-	isspace = false;
-	while (i < len)
+	if (trim->j > 0 && (!iswspace(trim->res[trim->j - 1]))) // if not the beginning of res and the prior space is not ws
+		trim->res[(trim->j)++] = ' '; // insert ws if there is none
+	trim->res[(trim->j)++] = input[trim->i]; // copying operator (not increasing i because of increase in function calling)
+	if ((input[trim->i] == '<' && input[trim->i + 1] == '<') || (input[trim->i] == '>' && input[trim->i + 1] == '>'))
 	{
-		if ((input[i] == 34) || (input[i] == 39)) // ascii for " and '
-			handle_ws_quotes(input, res, &i, &j);
-		else if ((input[i] == '<' && input[i + 1] == '<') || (input[i] == '>' && input[i + 1] == '>'))
+		trim->res[trim->j++] = input[trim->i + 1]; //copy both operators
+		trim->res[trim->j++] = ' ';
+		trim->i++;
+		return ;
+	}
+	if (input[trim->i] != '$')
+		trim->res[trim->j++] = ' ';
+}
+
+void	handle_special(t_trim *trim, char *input)
+{
+	char tmp;
+
+	if ((input[trim->i] == '\'') || (input[trim->i] == '\"'))
 		{
-			handle_double_op(input, res, &i, &j);
-			isspace = true;
+			handle_quotes(trim, input);
+			trim->isspace = true;
 		}
-		else if (input[i] == '|' || input[i] == '>' || input[i] == '<')
+	else if (input[trim->i] == '|' || input[trim->i] == '>' || input[trim->i] == '<' || input[trim->i] == '$')
 		{
-			handle_op(input, res, &i, &j);
-			isspace = true;
+			tmp = input[trim->i];
+			handle_op(trim, input);
+			if (tmp == '$')
+				trim->isspace = false;
+			else
+				trim->isspace = true;
 		}
-		else if (input[i] == ' ' || input[i] == '\n' || input[i] == '\t')
+}
+
+void	populate_str(t_trim *trim, char *input)
+{
+	trim->j = 0;
+	trim->i = 0;
+	trim->isspace = false; //flag to false
+	while (trim->i < trim->len)
+	{
+		if (isspecial(input[trim->i]))
+			handle_special(trim, input);
+		else if (iswspace(input[trim->i])) // checks if there is a ws in input and sets it to one ws
 		{
-			if (!isspace && j > 0)
+			if (!trim->isspace && trim->j > 0) // checks if isspace is false
 			{
-				res[j++] = ' ';
-				isspace = true;
+				trim->res[trim->j++] = ' '; // inserts space
+				trim->isspace = true; // sets flag to true
 			}
 		}
 		else
 		{
-			res[j++] = input[i];
-			isspace = false;
+			trim->res[trim->j++] = input[trim->i];
+			trim->isspace = false;
 		}
-		i++;
+		trim->i++;
 	}
-	res[j] = '\0';
-	return (res);
+	trim->res[trim->j] = '\0';
+}
+
+char *trim_spaces(char *input)
+{
+	t_trim	trim;
+	char	*trim_inpt;
+
+	if (!input)
+		return (NULL);
+	trim_inpt = ft_strtrim(input, " \n\t");
+	trim.len = ft_strlen(trim_inpt);
+	printf("len: %zu\n", trim.len);
+	trim.res = ft_calloc(sizeof(char), (trim.len + 1)); // calculate len properly?
+	if (!trim.res)
+		return (NULL);
+	populate_str(&trim, input);
+	printf("len: %zu\n", ft_strlen(trim.res));
+	return (trim.res);
 }
 
 int	main()
@@ -106,17 +148,14 @@ int	main()
 	char	*trim_inpt;
 	char	*res;
 
-	int i;
-	i = 0;
 	input = readline("Type Shit: ");
 	if(!input)
 		return (1);
 	trim_inpt = ft_strtrim(input, " \n\t");
 	res = trim_spaces(trim_inpt);
-	printf("%s\n", input);
-	printf("%s\n", trim_inpt);
 	printf("%s\n", res);
+	free(input);
+	free(trim_inpt);
+	free(res);
 	return (0);
 }
-
-// handle $
